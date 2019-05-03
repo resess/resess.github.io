5e973f879dfb843eebcfdbcee5553aa6397b6f30
dorawyy <dora93wang@gmail.com>
2019-05-03 12:08:21 -0700

    sync text from google doc to htmls

--- ResessWelcome/515e684a672516a0d06fd3f769a69ffec1e6f9c5/js/script.js
+++ ResessWelcome/515e684a672516a0d06fd3f769a69ffec1e6f9c5/js/script.js
@@ -5,7 +5,7 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
+        this.classList.toggle("active2");
         var content = this.nextElementSibling;
         if (content.style.maxHeight){
             content.style.maxHeight = null;
