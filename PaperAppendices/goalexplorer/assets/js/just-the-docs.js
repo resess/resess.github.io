581b2ba9e48b4a956aafab20ee6fb27191008f4f
Duling Lai <laiduling@gmail.com>
2019-09-14 12:00:30 +0800

    fixed links

--- PaperAppendices/goalexplorer/assets/js/just-the-docs.js
+++ PaperAppendices/goalexplorer/assets/js/just-the-docs.js
@@ -44,7 +44,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://resess.github.io/PaperAppendices/goalexplorer//assets/js/search-data.json', true);
+  request.open('GET', 'https://resess.github.io/PaperAppendices/goalexplorer/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
