1a3e263eb8bfafc97359885a137d5c9cc353f1c9
Yingying <dora93wang@gmail.com>
2024-10-24 09:25:39 +0800

    microservice study scripts

--- artifacts/MicroserviceToolStudy/assets/data/metrics/scripts/calculator/calculator.py
+++
@@ -1,9 +0,0 @@
-from models.decomposition_identifier import DecompositionIdentifier
-
-
-class Calculator:
-    def calculate_one(self, decomposition_identifier: DecompositionIdentifier):
-        pass
-
-    def calculate_all(self):
-        pass
