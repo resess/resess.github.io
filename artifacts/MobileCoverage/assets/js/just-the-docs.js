4bc7c7157087ee7fcab82b1eab44e25f07d50601
Faridah Akinotcho <faridath.akinotcho@yahoo.fr>
2025-01-09 07:18:40 -0800

    fixed rel links

--- artifacts/MobileCoverage/assets/js/just-the-docs.js
+++ artifacts/MobileCoverage/assets/js/just-the-docs.js
@@ -44,7 +44,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://https://resess.github.io/artifacts/MobileCoverage/assets/js/search-data.json', true);
+  request.open('GET', 'https://resess.github.io/artifacts/MobileCoverage/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
